72c8bd2ecfe8835d1343ba0b294d231b4ffc01ab
Immanuel Bayu <bayu.immanuel@gmail.com>
2017-08-06 13:28:27 +0700

    Update Logic Bot : One Filed Used

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const fs = require('fs');
 const opn = require('opn');
 
 let urlData = '';
-let max = 2, min = 1;
+let max = 0.2, min = 0.1;
 let randomMinute = (Math.floor(Math.random() * (max - min + 1)) + min) * 60 * 1000;
+
+let execKillBrowser = require('child_process').exec;
+let timeOutKillBrowser = 2 * 60 * 1000;
+let childKillBrowser;
+
+let execChangeVpn = require('child_process').exec;
+let childChangeVpn;
+let timeOutChangeVpn = 1 * 60 * 1000;
 
 fs.readFile('blog.txt', 'utf8', function (err,data) {
 	urlData = data.split(";");
 });
 
-function timeout() {
+function browserTimeout() {
     setTimeout(function () {
     	console.log("First interval Func : " + randomMinute);
 
@@ -22,23 +30,49 @@ function timeout() {
 		randomMinute = (Math.floor(Math.random() * (max - min + 1)) + min) * 60 * 1000;
 		console.log("Second interval Func : " + randomMinute);
 
-		let browserType = [];
+		let browserType = [['opera'], ['google-chrome', '--incognito'], ['firefox', '-private-window']];
+		let randomBrowser = Math.floor((Math.random() * (2 - 0 + 1)));	
 
-		if(random % 2 == 1){
-			console.log('Browser : Firefox');
-			browserType = ['firefox', '-private-window'];
-		} else if(random % 3 == 1) {
-			console.log('Browser : Opera');
-			browserType = ['opera'];
-		} else {
-			console.log('Browser : Chrome');
-			browserType = ['google-chrome', '--incognito'];
-		}
+		console.log("Use Browser : " + browserType[randomBrowser]);
 
-	  	opn(urlData[random].trim(), {app: browserType});
+	  	// opn(urlData[random].trim(), {app: browserType[randomBrowser]});
 
-        timeout();
+        browserTimeout();
     }, randomMinute);
 };
 
-timeout();
+function killBrowserTimeout() {
+    setTimeout(function () {
+    	childKillBrowser = execKillBrowser("./kill_browser.sh", function (error, stdout, stderr) {
+		  console.log('stdout: ' + stdout);
+		  console.log('stderr: ' + stderr);
+
+		  if (error !== null) {
+		    console.log('exec error: ' + error);
+		  }
+		});
+
+        killBrowserTimeout();
+    }, timeOutKillBrowser);
+};
+
+function changeVpnTimeout() {
+    setTimeout(function () {
+    	childChangeVpn = execChangeVpn("./express.sh", function (error, stdout, stderr) {
+		  console.log('stdout: ' + stdout);
+		  console.log('stderr: ' + stderr);
+		  
+		  if (error !== null) {
+		    console.log('exec error: ' + error);
+		  }
+
+		});
+
+        changeVpnTimeout();
+    }, timeOutChangeVpn);
+};
+
+
+browserTimeout();
+changeVpnTimeout();
+killBrowserTimeout();
