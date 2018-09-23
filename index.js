1e6e3844688bcaa6be25d1c480b44a12273f3d8c
Bayu <bayu.immanuel@gmail.com>
2018-09-24 00:00:21 +0700

    test

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ function browserTimeout() {
 		randomMinute = (Math.floor(Math.random() * (max - min + 1)) + min) * 60 * 1000;
 		console.log("Second interval Func : " + randomMinute);
 
-		// let browserType = [['opera'], ['google-chrome', '--incognito'], ['firefox', '-private-window']];
-		let browserType = [['firefox', '-private-window']];
+		let browserType = [['opera'], ['google-chrome', '--incognito'], ['firefox', '-private-window']];
 		let randomBrowser = Math.floor((Math.random() * (2 - 0 + 1)));	
 
 		console.log("Use Browser : " + browserType[randomBrowser]);
